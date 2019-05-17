082fc87664ffa19395c4ae2d02e793c19de9bbc1
indigomx9 <indigomx9@football-mx9.fios-router.home>
2019-05-17 11:56:21 -0400

    A practice app using React, Redux and TypeScript.

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <main>
+            <h1 className="banner">About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem rerum autem, quis, cumque expedita blanditiis velit voluptates consequuntur animi repudiandae vero. Cum velit voluptate beatae labore asperiores libero assumenda similique!</p>
+        </main>
+    );
+};
+
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Home = () => {
+    return (
+        <main>
+            <h1 className="banner">Home</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem rerum autem, quis, cumque expedita blanditiis velit voluptates consequuntur animi repudiandae vero. Cum velit voluptate beatae labore asperiores libero assumenda similique!</p>
+        </main>
+    );
+};
+
